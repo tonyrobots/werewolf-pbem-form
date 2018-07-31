2b00d046096cb2baf9eaf54614ba25e8cb13fbcc
Tony Zito <tonyzito@gmail.com>
2018-07-31 12:30:49 -0700

    bumped version number

--- werewolf.php
+++ werewolf.php
@@ -49,7 +49,7 @@ $bgcolor = sprintf( "#%02X", mt_rand( 0x88, 0xFF )) . sprintf( "%02X", mt_rand( 
 </head>
 <body bgcolor="<? echo $bgcolor ?>">
 <font size="+2"><b>Werewolf PbEM Submission Form</b></font><br />
-<font size="-1" color="#555555"><i>v. 1.2 -- if it's your first time here, or you're looking for a concise run-down of the rules, <a href="ww_intro.php">click here.</a></i></font>
+<font size="-1" color="#555555"><i>v. 1.3 -- if it's your first time here, or you're looking for a concise run-down of the rules, <a href="ww_intro.php">click here.</a></i></font>
 <br />
 <? if ($version == "2") { ?>
 <b>NOTE: THIS FORM IS CURRENTLY POINTING TO AN EXPERIMENTAL VERSION OF WEREWOLF</b>
